3b6162380383a788b2cb9abf2da00ef2bfc3f342
Gizmo <anvaradizov100@gmail.com>
2023-06-15 17:06:09 +0500

    section-5Ready

--- js/script.js
+++ js/script.js
@@ -141,89 +141,145 @@ filterBtn.addEventListener('click', () => {
 })
 
 
+// const sliderLine = document.querySelector('.popularItem__line');
+
+// const sliderItem = document.createElement('div')
+// sliderItem.classList.add('popularItem__card', 'slider__item', 'card-3')
+
+// sliderItem.innerHTML = `
+//     <div class="card-3__start">
+//         <div class="card-3__place">
+//             <img src="img/icons/cup.svg" alt="image">
+//             #1
+//         </div>
+//         <div class="card-3__links">
+//             <button class="card-3__subscribe"></button>
+//             <a href="#" class="card-3__goToSeller"></a>
+//         </div>
+//     </div>
+//     <hr class="devider">
+//     <div class="card-3__end">
+//         <div class="card-3__img">
+//             <img src="img/avatars/avatarBg-large.png" alt="" class="avatar">
+//             <span class="card-3__premiumIcon"></span>
+//         </div>
+//         <h3 class="card-3__name">Payton Harris</h3>
+//         <h5 class="card-3__earning section__text-1">2.456<span>eth</span></h5>
+//     </div>
+// `    
+// let arr = []
+// for (let index = 0; index < 10; index++) {
+//     arr.push(sliderItem)
+// }
+// console.log(arr);
+// for (let index = 0; index < 10; index++) {
+//     sliderLine.appendChild(arr[index])
+// }
 
 
 
 
 
-const slider = document.querySelector('.popularItem__slider'),
-    track = slider.querySelector('.popularItem__line'),
-    items = [...slider.querySelectorAll('.slider__item')],
-    btnPrev = [...document.querySelectorAll('.popularItem__button-prev')],
-    btnNext = [...document.querySelectorAll('.popularItem__button-next')];
-
-if (window.innerWidth <= 850) {
-    track.setAttribute('data-slidesToShow', 3)
-} if (window.innerWidth <= 600) {
-    track.setAttribute('data-slidesToShow', 2)
-} if (window.innerWidth <= 450) {
-    track.setAttribute('data-slidesToShow', 1)
-}
-position = 0
-slidesToShow = window.innerWidth >= 850 ? 4 : track.getAttribute('data-slidesToShow')
-slidesToScroll = slidesToShow
-itemsCount = items.length
-gap = 32
-
-itemWidth = Math.ceil((slider.clientWidth - gap * (slidesToShow - 1)) / slidesToShow);
-movePosition = (slidesToScroll * itemWidth) + (slidesToScroll * gap);
-
-items.forEach(element => {
-    element.style = `min-width: ${itemWidth}px;`
-});
-
-// window.addEventListener('resize', () => {
-//     window.location.reload()
-// })
-
-checkBtn()
-
-btnPrev.forEach(element => {
-    element.addEventListener('click', () => {
-        itemsLeft = Math.floor(Math.abs(position) / itemWidth)
-        position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth + itemsLeft * gap
-        setPosition()
-    })
-});
 
 
-btnNext.forEach(element => {
-    element.addEventListener('click', () => {
-        itemsLeft = itemsCount - Math.floor((Math.abs(position) + slidesToShow * itemWidth - slidesToShow * gap) / itemWidth);
-        position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth + itemsLeft * gap
-        setPosition()
-    })
 
-});
+class SliderMnBtns {
+    constructor(obj) {
+        this.slider = document.querySelector(obj.slider),
+            this.track = this.slider.querySelector(obj.track),
+            this.items = [...this.slider.querySelectorAll(obj.items)],
+            this.btnPrev = document.querySelectorAll(obj.btnPrev),
+            this.btnNext = document.querySelectorAll(obj.btnNext);
 
-function setPosition() {
-    track.style = `transform: translateX(${position}px)`
-    checkBtn()
-}
 
-function checkBtn() {
-    btnPrev.disabled = position === 0
-    btnNext.disabled = position <= - (itemsCount - slidesToShow) * itemWidth
-
-    btnPrev.forEach(element => {
-        if (position === 0) {
-            element.classList.remove('active')
-        } else {
-            element.classList.add('active')
+        if (window.innerWidth <= obj.media_1) {
+            this.track.setAttribute('data-slidesToShow', obj.data_1)
+        } if (window.innerWidth <= obj.media_2) {
+            this.track.setAttribute('data-slidesToShow', obj.data_2)
+        } if (window.innerWidth <= obj.media_3) {
+            this.track.setAttribute('data-slidesToShow', obj.data_3)
         }
-    });
 
-    btnNext.forEach(element => {
-        if (position <= - (itemsCount - slidesToShow) * itemWidth) {
-            element.classList.remove('active')
-        } else {
-            element.classList.add('active')
-        }
-    });
+        this.position = 0
+        this.slidesToShow = window.innerWidth >= obj.media_1 ? obj.slidesToShow : this.track.getAttribute('data-slidesToShow')
+        this.slidesToScroll = this.slidesToShow
+        this.itemsCount = this.items.length
+        this.gap = obj.gap
 
+        this.itemWidth = Math.ceil((this.slider.clientWidth - this.gap * (this.slidesToShow - 1)) / this.slidesToShow);
+        this.movePosition = (this.slidesToScroll * this.itemWidth) + (this.slidesToScroll * this.gap);
+
+        this.items.forEach(element => {
+            element.style = `min-width: ${this.itemWidth}px;`
+        });
+        
+        this.checkBtn()
+
+        this.btnPrev.forEach(element => {
+            element.addEventListener('click', () => {
+                this.itemsLeft = Math.floor(Math.abs(this.position) / this.itemWidth)
+                this.position += this.itemsLeft >= this.slidesToScroll ? this.movePosition : this.itemsLeft * this.itemWidth + this.itemsLeft * this.gap
+                this.setPosition()
+            })
+        });
+
+        this.btnNext.forEach(element => {
+            element.addEventListener('click', () => {
+                this.itemsLeft = this.itemsCount - Math.ceil((Math.abs(this.position) + this.slidesToShow * this.itemWidth - this.slidesToShow * this.gap) / this.itemWidth);
+                this.position -= this.itemsLeft >= this.slidesToScroll ? this.movePosition : this.itemsLeft * this.itemWidth + this.itemsLeft * this.gap
+                this.setPosition()
+            })
+        });
+
+    }
+
+    setPosition() {
+        this.track.style = `transform: translateX(${this.position}px)`
+        this.checkBtn()
+    }
+
+    checkBtn() {
+        this.btnPrev.forEach(element => {
+            element.disabled = this.position === 0
+        });
+        this.btnNext.forEach(element => {
+            element.disabled = this.position <= - (this.itemsCount - this.slidesToShow) * this.itemWidth
+        });
+
+        
+        this.btnPrev.forEach(element => {
+            if (this.position === 0) {
+                element.classList.remove('active')
+            } else {
+                element.classList.add('active')
+            }
+        });
+
+        this.btnNext.forEach(element => {
+            if (this.position <= - (this.itemsCount - this.slidesToShow) * this.itemWidth) {
+                element.classList.remove('active')
+            } else {
+                element.classList.add('active')
+            }
+        });
+
+    }
 }
-
-
+const section_3Slide = new SliderMnBtns({
+    slider: '.popularItem__slider',
+    track: '.popularItem__line',
+    items: '.slider__item',
+    btnPrev: '.popularItem__button-prev',
+    btnNext: '.popularItem__button-next',
+    slidesToShow: 4,
+    media_1: 850,
+    data_1: 3,
+    media_2: 600,
+    data_2: 2,
+    media_3: 450,
+    data_3: 1,
+    gap: 32,
+})
 
 class SliderMn {
     constructor(obj) {
@@ -248,7 +304,6 @@ class SliderMn {
         this.gap = obj.gap
 
         this.itemWidth = Math.ceil((this.slider.clientWidth - this.gap * (this.slidesToShow - 1)) / this.slidesToShow);
-        console.log(this.itemWidth);
         this.movePosition = (this.slidesToScroll * this.itemWidth) + (this.slidesToScroll * this.gap);
 
         this.items.forEach(element => {
@@ -309,7 +364,41 @@ const section_4Slide = new SliderMn({
     gap: 32,
 })
 
-console.log(section_4Slide);
+const section_5Slide = new SliderMnBtns({
+    slider: '.section-5__slider',
+    track: '.section-5__sliderTrack',
+    items: '.slider-5__item',
+    btnPrev: '.section-5__btn-prev',
+    btnNext: '.section-5__btn-next',
+    slidesToShow: 3,
+    media_1: 1100,
+    data_1: 2,
+    media_2: 800,
+    data_2: 2,
+    media_3: 650,
+    data_3: 1,
+    gap: 32,
+})
+
+const items = [...document.querySelectorAll('.popularItem__card')]
+items.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        removeActive(items)
+        element.addEventListener('mouseover', () => element.classList.add('active'))
+    })
+    element.addEventListener('mouseleave', () => {
+        removeActive(items)
+        element.addEventListener('mouseover', () => element.classList.remove('active'))
+    })
+});
+
+function removeActive(array) {
+    array.forEach(element => {
+        element.classList.remove('active')
+    });
+}
+
+
 
 
 
