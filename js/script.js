c11d6d74f0038425cce1af1c0e932482a43d4715
Gizmo <anvaradizov100@gmail.com>
2023-06-24 23:22:24 +0500

    sortBlock

--- js/script.js
+++ js/script.js
@@ -1,106 +1,4 @@
 
-class Slider {
-    constructor(obj) {
-        slider = document.querySelector(obj.slider)
-        sliderLine = slider.querySelector(obj.sliderLine)
-        slides = [...sliderLine.children]
-        next = slider.querySelector(obj.next)
-        prev = slider.querySelector(obj.prev)
-
-        width = slider.clientWidth
-        height = slider.clientHeight
-        dir = obj.direction.toUpperCase() == 'X' ? 'X' : 'Y'
-        moveSize = dir === 'X' ? width : height
-
-        activeSlide = 0
-
-        sliderLine.style = `
-            position: relative;
-            height: ${height}px;
-            overflow: hidden;
-        `
-
-        slides.forEach((element, index) => {
-            element.style = `
-                height: ${height}px;
-                width: ${width}px;
-                position: absolute;
-            `
-            if (index != activeSlide) {
-                element.style.transform = `translate${dir}(${moveSize}px)`
-            }
-
-            if (index === slides.length - 1) {
-                element.style.transform = `translate${dir}(-${moveSize}px)`
-            }
-        });
-
-        let interval = setInterval(() => {
-            move(next)
-        }, 2500);
-
-        slider.addEventListener('mouseover', () => {
-            clearInterval(interval)
-        })
-
-        slider.addEventListener('mouseleave', () => {
-            interval = setInterval(() => {
-                move(next)
-            }, 2500);
-        })
-
-        prev.addEventListener('click', () => move(prev))
-        next.addEventListener('click', () => move(next))
-    }
-
-    move(btn) {
-        console.log(btn);
-        let btnLeftOrRight = btn == next ? moveSize * -1 : moveSize
-
-        slides.forEach((element, index) => {
-            element.style.transition = '0s'
-            if (index != activeSlide) {
-                element.style.transform = `translate${dir}(${btnLeftOrRight * -1}px)`
-            }
-        })
-
-        slides[activeSlide].style.transform = `translate${dir}(${btnLeftOrRight}px)`
-        slides[activeSlide].style.transition = '1s'
-
-        if (btn === next) {
-            activeSlide++
-            if (activeSlide >= slides.length) {
-                activeSlide = 0
-            }
-        } else if (btn == prev) {
-            activeSlide--
-            if (activeSlide < 0) {
-                activeSlide = slides.length - 1
-            }
-        }
-
-        slides[activeSlide].style.transform = `translate${dir}(0px)`
-        slides[activeSlide].style.transition = '1s'
-
-        prev.setAttribute('disabled', 'disabled')
-        next.setAttribute('disabled', 'disabled')
-
-        setTimeout(() => {
-            prev.removeAttribute('disabled')
-            next.removeAttribute('disabled')
-        }, 1200);
-
-    }
-}
-
-// const slider = new Slider({
-//     slider: '.section-1__slider',
-//     sliderLine: '.section-1__sliderLine',
-//     next: '.section-1__ctrl-right',
-//     prev: '.section-1__ctrl-left',
-//     direction: 'X'
-// })
-
 let isMobile = {
     Android: function () { return navigator.userAgent.match(/Android/i); },
     BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
@@ -115,72 +13,6 @@ if (isMobile.any()) {
 } else {
     body.classList.add('_screen')
 }
-
-const filter = document.querySelector('.filter'),
-    filterBtn = filter.querySelector('.filter__btn'),
-    filterOptions = filter.querySelectorAll('.filter__option');
-
-
-
-filterBtn.addEventListener('click', () => {
-    filter.classList.toggle('active')
-    window.addEventListener('click', (e) => {
-        filterOptions.forEach(element => {
-            if (e.target == element) {
-                filterOptions.forEach(element => {
-                    element.classList.remove('active')
-                });
-                e.target.classList.add('active')
-                filterBtn.innerHTML = e.target.innerHTML
-                filter.classList.remove('active')
-            } else if (e.target != filterBtn) {
-                filter.classList.remove('active')
-            }
-        });
-    })
-})
-
-
-// const sliderLine = document.querySelector('.popularItem__line');
-
-// const sliderItem = document.createElement('div')
-// sliderItem.classList.add('popularItem__card', 'slider__item', 'card-3')
-
-// sliderItem.innerHTML = `
-//     <div class="card-3__start">
-//         <div class="card-3__place">
-//             <img src="img/icons/cup.svg" alt="image">
-//             #1
-//         </div>
-//         <div class="card-3__links">
-//             <button class="card-3__subscribe"></button>
-//             <a href="#" class="card-3__goToSeller"></a>
-//         </div>
-//     </div>
-//     <hr class="devider">
-//     <div class="card-3__end">
-//         <div class="card-3__img">
-//             <img src="img/avatars/avatarBg-large.png" alt="" class="avatar">
-//             <span class="card-3__premiumIcon"></span>
-//         </div>
-//         <h3 class="card-3__name">Payton Harris</h3>
-//         <h5 class="card-3__earning section__text-1">2.456<span>eth</span></h5>
-//     </div>
-// `    
-// let arr = []
-// for (let index = 0; index < 10; index++) {
-//     arr.push(sliderItem)
-// }
-// console.log(arr);
-// for (let index = 0; index < 10; index++) {
-//     sliderLine.appendChild(arr[index])
-// }
-
-
-
-
-
-
 
 
 class SliderMnBtns {
@@ -212,7 +44,7 @@ class SliderMnBtns {
         this.items.forEach(element => {
             element.style = `min-width: ${this.itemWidth}px;`
         });
-        
+
         this.checkBtn()
 
         this.btnPrev.forEach(element => {
@@ -225,8 +57,10 @@ class SliderMnBtns {
 
         this.btnNext.forEach(element => {
             element.addEventListener('click', () => {
-                this.itemsLeft = this.itemsCount - Math.ceil((Math.abs(this.position) + this.slidesToShow * this.itemWidth - this.slidesToShow * this.gap) / this.itemWidth);
+                this.itemsLeft = this.itemsCount - Math.round((Math.abs(this.position) + this.slidesToShow * this.itemWidth - this.slidesToShow * this.gap) / this.itemWidth);
+                console.log(this.itemsLeft);
                 this.position -= this.itemsLeft >= this.slidesToScroll ? this.movePosition : this.itemsLeft * this.itemWidth + this.itemsLeft * this.gap
+                console.log(this.position);
                 this.setPosition()
             })
         });
@@ -246,7 +80,7 @@ class SliderMnBtns {
             element.disabled = this.position <= - (this.itemsCount - this.slidesToShow) * this.itemWidth
         });
 
-        
+
         this.btnPrev.forEach(element => {
             if (this.position === 0) {
                 element.classList.remove('active')
@@ -380,27 +214,218 @@ const section_5Slide = new SliderMnBtns({
     gap: 32,
 })
 
-const items = [...document.querySelectorAll('.popularItem__card')]
-items.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        removeActive(items)
-        element.addEventListener('mouseover', () => element.classList.add('active'))
-    })
-    element.addEventListener('mouseleave', () => {
-        removeActive(items)
-        element.addEventListener('mouseover', () => element.classList.remove('active'))
-    })
-});
+// const section_6Slide = new SliderMn({
+//     slider: '.discover__slider',
+//     track: '.discover__track',
+//     items: '.discover__card',
+//     btnPrev: '.discoverSlider__button-prev',
+//     btnNext: '.discoverSlider__button-next',
+//     slidesToShow: 3,
+//     media_1: 1100,
+//     data_1: 2,
+//     media_2: 600,
+//     data_2: 2,
+//     media_3: 500,
+//     data_3: 1,
+//     gap: 32,
+// })
 
-function removeActive(array) {
-    array.forEach(element => {
-        element.classList.remove('active')
-    });
+if (window.innerWidth <= 600) {
+    const slider = document.querySelector('.discover__slider'),
+        track = slider.querySelector('.discover__track'),
+        items = [...slider.querySelectorAll('.discover__card')],
+        btnPrev = document.querySelector('.discoverSlider__button-prev'),
+        btnNext = document.querySelector('.discoverSlider__button-next');
+    
+        
+        let position = 0
+        let slidesToShow = window.innerWidth >= 1
+        let slidesToScroll = slidesToShow
+        let itemsCount = items.length
+        let gap = 16
+        
+        
+        let itemWidth = Math.ceil((slider.clientWidth - gap * (slidesToShow - 1)) / slidesToShow);
+        let movePosition = (slidesToScroll * itemWidth) + (slidesToScroll * gap);
+        
+        items.forEach(element => {
+            element.style = `min-width: ${itemWidth}px;`
+        });
+        
+        checkBtn()
+        
+        btnPrev.addEventListener('click', () => {
+            let itemsLeft = Math.floor(Math.abs(position) / itemWidth)
+            position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth + itemsLeft * gap
+            console.log(itemsLeft);
+            console.log(position);
+            setPosition()
+        })
+        
+        btnNext.addEventListener('click', () => {
+            let itemsLeft = itemsCount - Math.round((Math.abs(position) + slidesToShow * itemWidth - slidesToShow * gap) / itemWidth);
+            position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth + itemsLeft * gap
+            console.log(itemsLeft);
+            console.log(position);
+            setPosition()
+        })
+
+    function setPosition() {
+        track.style = `transform: translateX(${position}px)`
+        checkBtn()
+    }
+
+    function checkBtn() {
+        if (position === 0) {
+            btnPrev.classList.remove('active')
+            btnPrev.setAttribute('disabled','')
+        } else {
+            btnPrev.classList.add('active')
+        }
+        
+        if (position <= - (itemsCount - slidesToShow) * itemWidth) {
+            btnNext.classList.remove('active')
+            btnNext.setAttribute('disabled','')
+        } else {
+            btnNext.classList.add('active')
+        }
+
+    }
 }
 
 
+    
+class Filter {
+    constructor(obj) {
+        this.filter = document.querySelector(obj.filter),
+        this.btn = document.querySelector(obj.btn),
+        this.options = document.querySelectorAll(obj.options);
+        
+        
+        this.btn.addEventListener('click', () => {
+            this.filter.classList.toggle('active')
+            window.addEventListener('click', (e) => {
+                this.options.forEach(element => {
+                    if (e.target == element) {
+                        this.options.forEach(element => {
+                            element.classList.remove('active')
+                        });
+                        e.target.classList.add('active')
+                        this.btn.innerHTML = e.target.innerHTML
+                        this.filter.classList.remove('active')
+                    } else if (e.target != this.btn) {
+                        this.filter.classList.remove('active')
+                    }
+                });
+            })
+        })
+    }
+}
+
+const section_3Filter = new Filter({
+    filter: '.popelarItem__filter',
+    btn: '.popelarItem__filterBtn',
+    options: '.filter__option',
+})
+
+const section_6Filter_1 = new Filter({
+    filter: '.filter-6',
+    btn: '.filter-6__btn',
+    options: '.filter-6__option',
+})
+
+const section_62Filter_1 = new Filter({
+    filter: '.discover-settings__filters-1 .filter-62',
+    btn: '.discover-settings__filters-1 .filter-62__btn',
+    options: '.discover-settings__filters-1 .filter-62__option',
+})
+const section_62Filter_2 = new Filter({
+    filter: '.discover-settings__filters-2 .filter-62',
+    btn: '.discover-settings__filters-2 .filter-62__btn',
+    options: '.discover-settings__filters-2 .filter-62__option',
+})
+const section_62Filter_3 = new Filter({
+    filter: '.discover-settings__filters-3 .filter-62',
+    btn: '.discover-settings__filters-3 .filter-62__btn',
+    options: '.discover-settings__filters-3 .filter-62__option',
+})
+
+const discoverSettingsStart = document.querySelector('.discover-settings__start'),
+    categoriesData = [...document.querySelectorAll('.discover-settings__category')];
+let filterBlock = document.createElement('div')
+filterBlock.classList.add('filter-6', 'filter', 'filter-6-media')
+filterBlock.innerHTML = `
+    <button class="filter-6__btn-media filter-6__btn filter__btn filter__text">${categoriesData[0].textContent}</button>
+    <ul class="filter-6__popUp-media filter-6__popUp filter__popUp">
+        <li class="filter-6__option-media filter-6__option filter__option filter__text active">${categoriesData[0].textContent}</li>
+        <li class="filter-6__option-media filter-6__option filter__option filter__text">${categoriesData[1].textContent}</li>
+        <li class="filter-6__option-media filter-6__option filter__option filter__text">${categoriesData[2].textContent}</li>
+        <li class="filter-6__option-media filter-6__option filter__option filter__text">${categoriesData[3].textContent}</li>
+        <li class="filter-6__option-media filter-6__option filter__option filter__text">${categoriesData[4].textContent}</li>
+        <li class="filter-6__option-media filter-6__option filter__option filter__text">${categoriesData[5].textContent}</li>
+    </ul>
+`
+discoverSettingsStart.firstElementChild.after(filterBlock)
+const categoriesBlock = document.querySelector('.discover-settings__categories');
+
+if (window.innerWidth > 768) {
+    filterBlock.style.display = 'none'
+} else {
+    filterBlock.style.display = 'flex'
+}
+
+
+const section_62Filter_media= new Filter({
+    filter: '.filter-6-media',
+    btn: '.filter-6__btn-media',
+    options: '.filter-6__option-media',
+})
+
+if (body.classList.contains('_screen')) {
+    const items = [...document.querySelectorAll('.popularItem__card')]
+    items.forEach(element => {
+        element.addEventListener('mouseover', () => {
+            removeActive(items)
+            element.addEventListener('mouseover', () => element.classList.add('active'))
+        })
+        element.addEventListener('mouseleave', () => {
+            removeActive(items)
+            element.addEventListener('mouseover', () => element.classList.remove('active'))
+        })
+    });
+    function removeActive(array) {
+        array.forEach(element => {
+            element.classList.remove('active')
+        });
+    }
+}
+
+
+function addRemoveAcrive(array) {
+    array.forEach(element => {
+        element.addEventListener('click', (e) => {
+            e.preventDefault()
+            array.forEach(element => {
+                element.classList.remove('active')
+            });
+            element.classList.add('active')
+        })
+    });
+}
+
+const categories = document.querySelectorAll('.discover-settings__category')
+addRemoveAcrive(categories)
 
 
 
+
+const settingsButton = document.querySelector('.discover-settings__btn'),
+    settingsBlock = document.querySelector('.discover__settings');
+
+
+settingsButton.addEventListener('click', () => {
+    settingsButton.classList.toggle('active')
+    settingsBlock.classList.toggle('active')
+})
 
 
